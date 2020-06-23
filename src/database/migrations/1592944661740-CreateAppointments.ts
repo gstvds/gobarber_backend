342739497b741ab9ff7c855433015693a22edf59
Gustavo Silva <gstvds@icloud.com>
2020-06-23 20:24:39 -0300

    feat: Adds appointments repository based on typeorm repositories

--- src/database/migrations/1592944661740-CreateAppointments.ts
+++ src/database/migrations/1592944661740-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1592944661740
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
