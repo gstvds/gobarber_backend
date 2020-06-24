b4d4ad2f083e7dc235b666c6c910277695d6500c
Gustavo Silva <gstvds@icloud.com>
2020-06-24 20:32:02 -0300

    fix: Changes users and appointments migrations

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,4 +1,15 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+/* eslint-disable camelcase */
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
@@ -6,10 +17,20 @@ class Appointment {
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
 export default Appointment;
