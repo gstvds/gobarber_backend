08b8f20e6a10f9040b426710d25393e2c79857e4
Gustavo Silva <gstvds@icloud.com>
2020-06-25 17:33:28 -0300

    feat: Adds global exception handler

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -10,19 +10,15 @@ import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 const usersRouter = Router();
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUserService = new CreateUserService();
+  const createUserService = new CreateUserService();
 
-    const user = await createUserService.execute({ name, email, password });
+  const user = await createUserService.execute({ name, email, password });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(user);
 });
 
 const upload = multer(uploadConfig);
@@ -31,19 +27,15 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatar_filename: request.file.filename,
-      });
+    const updateUserAvatar = new UpdateUserAvatarService();
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatar_filename: request.file.filename,
+    });
 
-      delete user.password;
+    delete user.password;
 
-      return response.json(user);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    return response.json(user);
   },
 );
 
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
-import express from 'express';
+
+import express, { Request, Response, NextFunction } from 'express';
 import routes from './routes';
 import uploadConfig from './config/upload';
 
 import './database';
+import AppError from './errors/AppError';
 
 const PORT = process.env.PORT || 3333;
 const app = express();
@@ -11,6 +13,22 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(PORT, () => {
   console.log('Server started on port', +PORT);
