21ae44574f5218cb8474f98355bd6c6f90b159a3
Gustavo Silva <gstvds@icloud.com>
2020-06-25 17:12:06 -0300

    feat: Adds new AppError clas

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -6,6 +6,8 @@ import fs from 'fs';
 import uploadConfig from '../config/upload';
 import User from '../models/User';
 
+import AppError from '../errors/AppError';
+
 interface Request {
   user_id: string;
   avatar_filename: string;
@@ -18,7 +20,7 @@ class UpdateUserAvatarService {
     const user = await userRepository.findOne(user_id);
 
     if (!user) {
-      throw new Error('Only authenticated users can change avatar.');
+      throw new AppError('Only authenticated users can change avatar.', 401);
     }
 
     if (user.avatar) {
