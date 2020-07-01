d5061738938ce563a6dcf65194c347f0a5cfb978
Gustavo Silva <gstvds@icloud.com>
2020-07-01 17:32:51 -0300

    feat: Adds cors and express-async-errors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
 import routes from './routes';
 import uploadConfig from './config/upload';
 
@@ -10,6 +12,7 @@ import AppError from './errors/AppError';
 const PORT = process.env.PORT || 3333;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
