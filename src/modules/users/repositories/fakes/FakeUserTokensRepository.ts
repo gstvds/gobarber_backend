70e8aa49fff171943158371abd41402ad0e66fc7
Gustavo Silva <gstvds@icloud.com>
2020-08-08 21:21:54 -0300

    feat: Adds fake UserTokens to use tests and refact SendForgotPassword tests to be less wordy

---
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -0,0 +1,29 @@
+import { uuid } from 'uuidv4';
+
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+
+class FakeUserTokensRepository implements IUserTokensRepository {
+  private userTokens: UserToken[] = [];
+
+  /**
+   * Busca um usuário pelo id
+   * @param id id do usuário
+   */
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      id: uuid(),
+      token: uuid(),
+      user_id,
+    });
+
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+}
+
+export default FakeUserTokensRepository;
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -4,6 +4,7 @@ import AppError from '@shared/errors/AppError';
 // import User from '@modules/users/infra/typeorm/entities/User';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 interface IRequest {
   email: string;
@@ -16,14 +17,18 @@ class SendForgotPasswordEmailPassword {
     private usersRepository: IUsersRepository,
     @inject('MailProvider')
     private mailProvider: IMailProvider,
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
-    const checkUserExists = await this.usersRepository.findByEmail(email);
+    const user = await this.usersRepository.findByEmail(email);
 
-    if (!checkUserExists) {
+    if (!user) {
       throw new AppError('User does not exists.');
     }
+
+    await this.userTokensRepository.generate(user.id);
 
     this.mailProvider.sendMail(
       email,
