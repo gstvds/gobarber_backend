1da5835211ff247caee4b711ea132e0d431724ce
Gustavo Silva <gstvds@icloud.com>
2020-08-10 18:35:39 -0300

    feat: Adds forgot password email template and parse it to send to user

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import path from 'path';
 
 import AppError from '@shared/errors/AppError';
 // import User from '@modules/users/infra/typeorm/entities/User';
@@ -30,6 +31,13 @@ class SendForgotPasswordEmailPassword {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
     await this.mailProvider.sendMail({
       to: {
         name: user.name,
@@ -37,10 +45,10 @@ class SendForgotPasswordEmailPassword {
       },
       subject: '[GoBarber] Recuperação de senha',
       templateData: {
-        template: 'Olá, {{name}}: {{token}}',
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     });
