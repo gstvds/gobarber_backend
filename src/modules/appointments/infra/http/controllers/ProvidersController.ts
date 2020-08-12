193ee38da9e0018494c263e88a71ffa9a2baf681
Gustavo Silva <gstvds@icloud.com>
2020-08-12 14:26:39 -0300

    feat: Adds ProvidersController to handle providers requests

---
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -0,0 +1,18 @@
+import { container } from 'tsyringe';
+import { Request, Response } from 'express';
+
+import ListProvidersService from '@modules/appointments/services/ListProvidersService';
+
+export default class ProvidersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+
+    const listProviders = container.resolve(ListProvidersService);
+
+    const appointment = await listProviders.execute({
+      user_id,
+    });
+
+    return response.json(appointment);
+  }
+}
