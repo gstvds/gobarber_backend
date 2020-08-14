ba376b1d0f6e0ba22aa725209d5d0020349db3aa
Gustavo Silva <gstvds@icloud.com>
2020-08-13 21:31:36 -0300

    feat: List available hours of a day from a provider

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -3,7 +3,6 @@ import { inject, injectable } from 'tsyringe';
 import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-// import User from '@modules/users/infra/typeorm/entities/User';
 
 interface IRequest {
   provider_id: string;
