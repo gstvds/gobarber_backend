41200a6fa6b753eb79555542b7f875ba8482720f
Gustavo Silva <gstvds@icloud.com>
2020-08-17 22:19:01 -0300

    fix: Fixes tests to add new column

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -17,6 +17,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointmentService.execute({
       date: new Date(),
+      user_id: '123123',
       provider_id: '123123',
     });
 
@@ -29,12 +30,14 @@ describe('CreateAppointment', () => {
 
     await createAppointmentService.execute({
       date: appointmentDate,
+      user_id: '123123',
       provider_id: '123123',
     });
 
     expect(
       createAppointmentService.execute({
         date: appointmentDate,
+        user_id: '123123',
         provider_id: '123123',
       }),
     ).rejects.toBeInstanceOf(AppError);
