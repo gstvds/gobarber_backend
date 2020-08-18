cb15f2811df5c21b034e8ad1a13276acd6d82a99
Gustavo Silva <gstvds@icloud.com>
2020-08-17 22:58:25 -0300

    feat: Adds controllers to handle availability methods

---
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,24 @@
+import { container } from 'tsyringe';
+import { Request, Response } from 'express';
+
+import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+
+export default class ProviderDayAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { day, month, year } = request.body;
+
+    const listProviderDayAvailability = container.resolve(
+      ListProviderDayAvailabilityService,
+    );
+
+    const availability = await listProviderDayAvailability.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+
+    return response.json(availability);
+  }
+}
