e477cdc4e932775ca2b22949b84c4f561c6dae6d
Gustavo Silva <gstvds@icloud.com>
2020-08-17 22:13:04 -0300

    feat: Adds user_id when creating a new appointment

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -54,10 +54,11 @@ class FakeAppointmentsRepository implements IAppointmentsRepository {
 
   public async create({
     provider_id,
+    user_id,
     date,
   }: ICreateAppointmentDTO): Promise<Appointment> {
     const appointment = new Appointment();
-    Object.assign(appointment, { id: uuid(), provider_id, date });
+    Object.assign(appointment, { id: uuid(), provider_id, date, user_id });
 
     this.appointments.push(appointment);
     return appointment;
