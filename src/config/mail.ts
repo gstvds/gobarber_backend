a7c8c495def9d9ba40c0b911305cbdf669033f9e
Gustavo Silva <gstvds@icloud.com>
2020-08-19 19:18:29 -0300

    feat: Adds connection to Amazon AWS SES to send emails

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'guustavoqb@gmail.com',
+      name: 'Gustavo Silva',
+    },
+  },
+} as IMailConfig;
