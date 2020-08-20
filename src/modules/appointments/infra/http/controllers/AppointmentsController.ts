4ae5b99a0e656c0ee716ce1a3b1ddf9031979774
Gustavo Silva <gstvds@icloud.com>
2020-08-20 18:51:13 -0300

    feat: Adds cache to ListProviderAppointments

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,4 +1,3 @@
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 import { Request, Response } from 'express';
 
@@ -9,14 +8,12 @@ export default class AppointmentsController {
     const user_id = request.user.id;
     const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
-
     const createAppointment = container.resolve(CreateAppointmentService);
 
     const appointment = await createAppointment.execute({
       provider_id,
       user_id,
-      date: parsedDate,
+      date,
     });
 
     return response.json(appointment);
